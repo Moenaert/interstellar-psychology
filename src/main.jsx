83bec8849d7ba7854c01fa0be9039af4cda56292
Pielie <gilles.1998.moenaert@gmail.com>
2025-10-31 02:07:05 +0100

    Add error handling and logging to debug white screen

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+console.log('Main.jsx loaded');
+
+const rootElement = document.getElementById('root');
+console.log('Root element:', rootElement);
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+  console.log('React app rendered');
+} else {
+  console.error('Root element not found!');
+  document.body.innerHTML = '<h1>Error: Root element not found</h1>';
+}
